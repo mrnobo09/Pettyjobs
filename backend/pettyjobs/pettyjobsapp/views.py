6b32a6fe68047ecc2b37040b7a0956dc1c241b1b
mrnobo09 <shahabkhanzada999@gmail.com>
2024-08-21 21:47:25 +0500

    Added djoser to backend \nDatabase setup pending

--- backend/pettyjobs/pettyjobsapp/views.py
+++ backend/pettyjobs/pettyjobsapp/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-def index(request):
-    return HttpResponse("Hello world")
+
+
+class Index(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
